22b9e494946c60182e535f9127859aefccb1f3a2
Arthur Brito Medeiros <47860925+ArthurBM@users.noreply.github.com>
2020-07-04 00:01:30 -0300

    Detalhes

--- main.py
+++ main.py
@@ -15,10 +15,62 @@ def fazer_dados():
 
     return nomes_completos
 
+def pedir_mensagem():
+    # All the stuff inside your window.
+    layout = [  [sg.Text('Escreva a mensagem')],
+                [sg.Multiline(size=(40,20), enable_events=True)],
+                [sg.Button('Ok')] ]
+
+    # Create the Window
+    window = sg.Window('Mensagem', layout)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Ok': # if user closes window or clicks ok
+            break
+    window.close()
+    return values[0]
+    
+
+def num_arquivos():
+    # All the stuff inside your window.
+    layout = [  [sg.Text('Quantos arquivos você quer enviar')],
+                [sg.InputText()],
+                [sg.Button('Ok')] ]
+
+    # Create the Window
+    window = sg.Window('Mensagem', layout)
+    # Event Loop to process "events" and get the "values" of the inputs
+
+    event, values = window.read()
+
+    print(values)
+    window.close()
+    return int(values[0])
+
+def pedir_arquivos(num_files):
+    layout = [  [sg.Text('Caminhos dos arquivos')]] 
+
+    list_filepath_send = []
+    for x in range(0,num_files):
+        layout.append([sg.Input(), sg.FileBrowse()])
+    
+    layout.append([sg.OK(), sg.Cancel()])
+
+    window = sg.Window('Arquivos', layout)
+
+    event, values = window.read()
+    for y in range(0,num_files):
+        list_filepath_send.append(values[y])
+
+    print(list_filepath_send)
+
+    window.close()
+
+    return list_filepath_send
+
 #LEMBRA DE COLOCAR O ÍNDICE DO PRIMEIRO NOME COMO 2
 def pedir_informacoes():
-    flag = False
-    flag2 =False
     opcao = 0
     opcao_mensagem = 0
     mensagem_inicio = textwrap.dedent(
@@ -34,80 +86,47 @@ def pedir_informacoes():
         )
     print(mensagem_inicio)
 
-    while ( (opcao != 1 and opcao != 2) or (flag == False) ):
+    while ( (opcao != 1 and opcao != 2)):
         opcao = int(input('Digite o número da opção escolhida para o formato que você quer fornecer o nome:\n1 - Arquivo CSV\n2 - Digitar os nomes\n'))
 
         if opcao == 1:
             contatos = fazer_dados()
-            flag = True
+            break
         elif opcao == 2:
             contatos = input("Digite os nomes exatamente como estão salvos nos contatos separados por uma /\nExemplo: Arthur Brito Medeiros/Pedro Medeiros/Isabela Campelo\n")
             contatos = contatos.split('/')
-            flag = True
+            break
         else:
             print('Opção inválida, escolha novamente')
         #nomes = fazer_dados()
 
-    while ( (opcao != 1 and opcao != 2 and opcao != 3) or (flag2 == False) ):
+    while ( (opcao_mensagem != 1 and opcao_mensagem != 2 and opcao_mensagem != 3)):
 
         opcao_mensagem = int(input('O que você deseja fazer:\n1 - Enviar mensagem e arquivo\n2 - Enviar só mensagem\n3 - Enviar só arquivo\n'))
 
         if opcao_mensagem == 1:
-            # All the stuff inside your window.
-            layout = [  [sg.Text('Some text on Row 1')],
-                        [sg.Multiline(size=(40,20))],
-                        [sg.Button('Ok')] ]
-
-            # Create the Window
-            window = sg.Window('Mensagem', layout)
-            # Event Loop to process "events" and get the "values" of the inputs
-            while True:
-                event, values = window.read()
-                if event == sg.WIN_CLOSED or event == 'Ok': # if user closes window or clicks cancel
-                    print('You entered ', values[0])
-                    break
-
+            mensagem = pedir_mensagem()
             
-            num_files = int(input('Quantos arquivos você quer enviar?\n'))
+            num_files = num_arquivos()
 
-            list_filepath_send = []
-            for list_filepath in range(0,num_files):
-                print('Escolha o arquivo')
-                list_filepath = askopenfile()
-                list_filepath_send.append(list_filepath.name)
-            
-            window.close()
-            mensagem = values[0]
+            list_filepath_send = pedir_arquivos(num_files)
+
             bot2 = WhatsappBot()
             bot2.enviarMensagemImagem(contatos,list_filepath_send, mensagem)
-            flag2 =True
             bot2.driver.quit()
             with open("number_msg_send.txt", "a") as myfile:
                 myfile.write(f"Número de mensagens enviadas: {bot2.count_mensagens}\n")
+            break
 
         elif opcao_mensagem == 2:
-            # All the stuff inside your window.
-            layout = [  [sg.Text('Some text on Row 1')],
-                        [sg.Multiline(size=(40,20))],
-                        [sg.Button('Ok')] ]
-
-            # Create the Window
-            window = sg.Window('Mensagem', layout)
-            # Event Loop to process "events" and get the "values" of the inputs
-            while True:
-                event, values = window.read()
-                if event == sg.WIN_CLOSED or event == 'Ok': # if user closes window or clicks cancel
-                    print('You entered ', values[0])
-                    break
-
-            window.close()
-            mensagem = values[0]
+            
+            mensagem = pedir_mensagem()
             bot2 = WhatsappBot()
             bot2.enviarMensagens(contatos, mensagem)
-            flag2 = True
             bot2.driver.quit()
             with open("number_msg_send.txt", "a") as myfile:
                 myfile.write(f"Número de mensagens enviadas: {bot2.count_mensagens}\n")
+            break
 
         elif opcao_mensagem == 3:
             num_files = int(input('Quantos arquivos você quer enviar?\n'))
@@ -119,10 +138,10 @@ def pedir_informacoes():
                 list_filepath_send2.append(list_filepath.name2)
             bot2 = WhatsappBot()
             bot2.enviarImagem(contatos, list_filepath_send2)
-            flag2 = True
             bot2.driver.quit()
             with open("number_msg_send.txt", "a") as myfile:
                 myfile.write(f"Número de mensagens enviadas: {bot2.count_mensagens}\n")
+            break
         else:
             print('Opção inválida, escolha novamente')
 
